3869f0cf80ac5bd584bd9effcbd0f4ba5cdda926
james williams & Toby Alden <student@epicodus.com>
2015-06-15 11:12:28 -0700

    added initial project files, post page and posts page

---
+++ server/mocks/posts.js
@@ -0,0 +1,84 @@
+if (!Array.prototype.find) {
+  Array.prototype.find = function(predicate) {
+    if (this == null) {
+      throw new TypeError('Array.prototype.find called on null or undefined');
+    }
+    if (typeof predicate !== 'function') {
+      throw new TypeError('predicate must be a function');
+    }
+    var list = Object(this);
+    var length = list.length >>> 0;
+    var thisArg = arguments[1];
+    var value;
+
+    for (var i = 0; i < length; i++) {
+      value = list[i];
+      if (predicate.call(thisArg, value, i, list)) {
+        return value;
+      }
+    }
+    return undefined;
+  };
+}
+
+var posts = [
+  {
+    id: 1,
+    title: 'bananas',
+    author: 1,
+    body: 'I am banana'
+  },
+  {
+    id: 2,
+    title: 'Apples',
+    author: 1,
+    body: "my spoon is to big"
+  }
+];
+
+var authors = [
+  {
+    id: 1,
+    name: "George",
+    posts: [1, 2]
+  }
+];
+
+module.exports = function(app) {
+  var express = require('express');
+  var postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': posts,
+      "authors": authors
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'post': posts.find(function(post) {
+        return post.id == req.params.id
+      }),
+      'authors': authors
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  app.use('/api/posts', postsRouter);
+};
